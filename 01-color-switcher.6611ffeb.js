b3c64bd4d0815519af20e8742eac1264da199407
Tsakhlo <97940139+Tsakhlo@users.noreply.github.com>
2022-08-16 18:05:39 +0000

    Deploying to gh-pages from @ Tsakhlo/goit-js-hw-09@135ebe5ad26051bb15cd6511be124a9fd622a477 🚀

---
+++ 01-color-switcher.6611ffeb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0}));var o=null;e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.6611ffeb.js.map
---
+++ 01-color-switcher.e532c96d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0}));let r=null;e.addEventListener("click",(()=>{clearInterval(r),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e532c96d.js.map
